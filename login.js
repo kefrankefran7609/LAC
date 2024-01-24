7f82a3781a2b41c84d8d6e99dd0eba3759a2f04a
kefrankefran7609 <48444561+kefrankefran7609@users.noreply.github.com>
2024-01-24 15:02:14 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,110 +1,74 @@
-var dundasCountry
-var dundasUserType
-window.onload = async () => {
-   function getCookie(name) {
-      var cookies = document.cookie.split('; ');
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].split('=');
-        if (cookie[0] === name) {
-          return decodeURIComponent(cookie[1]);
-        }
+console.log("LAC Hey login ")
+
+// Hide and show password 
+document.querySelector('[showpassword]').addEventListener('click', () => {
+    document.querySelector('[hidepass]').classList.toggle('hide')
+    document.querySelector('[showpass]').classList.toggle('opacity')
+    var pass = document.querySelector('[password]')
+    if(pass.type === "password") {
+        pass.type = "text";
+      } else {
+        pass.type = "password";
       }
-      return null;
+    })
+
+// Animation on input label 
+document.querySelectorAll('[input]').forEach((input) => {
+	input.addEventListener('focus', () => {
+  input.nextElementSibling.classList.add('active')
+ })
+ 
+ input.addEventListener('focusout', () => {
+ 	if(input.value == ""){
+  input.nextElementSibling.classList.remove('active')
+  }
+ })
+ })
+
+
+ document.querySelector('[login]').addEventListener('click', (e) => {
+    e.preventDefault()
+    // if both fields are filled, warnings if not
+    document.querySelector('[email]').classList.remove('empty'); 
+    document.querySelector('[password]').classList.remove('empty'); 
+    if(document.querySelector('[email]').value && document.querySelector('[password]').value) loginUser();
+    else {
+      if(!document.querySelector('[email]').value){
+    document.querySelector('[email]').classList.add('empty'); document.querySelector('[emptyfields]').classList.add('show'); setTimeout(() => document.querySelector('[emptyfields]').classList.remove('show'), 3000);
+    } if (!document.querySelector('[password]').value){
+    document.querySelector('[password]').classList.add('empty'); document.querySelector('[emptyfields]').classList.add('show'); setTimeout(() => document.querySelector('[emptyfields]').classList.remove('show'), 3000);
     }
-    dundasCountry = getCookie('dundasCountry')
-    dundasUserType = getCookie('dundasUserType')
-    if(dundasCountry){
-      document.querySelectorAll('[radioflag]').forEach(el => {
-      if(el.value == dundasCountry.replace(/_/g, ' ')) { 
-        el.parentElement.classList.add('is-active') // Make the radio-button for country active
-        document.querySelectorAll('[flag]').forEach(els => { // Update the flag in the nav
-            els.src = el.nextElementSibling.src
-           })
-           document.querySelectorAll('[flag]').forEach(els => {
-            els.srcset = el.nextElementSibling.srcset
-           })
-        document.querySelector('[country]').textContent = dundasCountry.replace(/_/g, ' ') // Update the name of the country in the nav
-    }})
-    } else {
-      document.querySelector('input[type="radio"][value="UK"]').parentElement.classList.add('is-active') // Make UK active if no cookie
-    } 
-    if(dundasUserType){
-      if(dundasCountry.replace(/_/g, ' ') == "Professional investor") {
-      document.querySelector('[professional]').classList.add('is-active')
-      document.querySelector('[investortype]').textContent = dundasCountry.replace(/_/g, ' ')
-      }
-      else {
-      document.querySelector('[nonprofessional]').classList.add('is-active')
-      document.querySelector('[investortype]').textContent = "Non-professional investor"
-      }
-    } else document.querySelector('[professional]').classList.add('is-active')
-}
-
-//Open nav main dropdown
-document.querySelector('[openmaindropdown]').addEventListener('click', () => {
-  document.querySelector('[maindropdown]').classList.toggle('show')
-  document.querySelector('[maindropdownarrow]').classList.toggle('show')
-})
-
-//Close nav main dropdown
-function closeDropdown(){
-document.querySelector('[closedropdown]').addEventListener('click', () => {
-  document.querySelector('[maindropdown]').classList.remove('show')
-  document.querySelector('[maindropdownarrow]').classList.remove('show')
-  document.querySelector('[locationdropdowncontent]').classList.remove('show') 
-  document.querySelector('[usertypedropdowncontent]').classList.remove('show') 
-})}
-closeDropdown()
-
-document.querySelector('[locationtoggle]').addEventListener('click', () => {
-  document.querySelector('[locationdropdowncontent]').classList.toggle('show') 
-})
-
-document.querySelector('[usertypetoggle]').addEventListener('click', () => {
-  document.querySelector('[usertypedropdowncontent]').classList.toggle('show') 
-})
-
-
-
-// Changing country
-function changeCountry(){
-document.querySelectorAll('[radioflag]').forEach(el => { // when a country is selected
-  el.addEventListener('change', (e) => {
-      if(!e.target.classList.contains('is-active')){
-       document.querySelectorAll('.radio-button').forEach(els => { // remove active class on all countries
-        els.classList.remove('is-active')
-       })  
-       e.target.parentElement.classList.add('is-active') // add active class on the selected one
-       document.querySelectorAll('[flag]').forEach(el => { // Update the flag in the nav
-        el.src = e.target.nextElementSibling.src
-       })
-       document.querySelectorAll('[flag]').forEach(el => {
-        el.srcset = e.target.nextElementSibling.srcset
-       })
-      document.querySelector('[country]').textContent = e.target.parentElement.lastChild.textContent // Update the country text in the nav
-       const country = encodeURIComponent(e.target.value.replace(/ /g, '_'))
-       document.cookie = `dundasCountry=${country}`                 
-      }
-  })
-})
-}
-changeCountry()
-
-
-// Selecting a user type
-function changeUserType(){
-document.querySelectorAll('.nav_top-dropdown-type').forEach(el => {
-  el.addEventListener('click', (e) => {
-  if(!e.target.classList.contains('is-active')){  
-  document.querySelectorAll('.nav_top-dropdown-type').forEach(els => {
-    els.classList.remove('is-active')
-  })
-  e.target.classList.add('is-active')
-  document.querySelector('[investortype]').textContent = e.target.id.replace(/_/g, ' ')
-  const userType = encodeURIComponent(e.target.id.replace(/_/g, ' '))
-  document.cookie = `dundasUserType=${userType}`
   }
   })
-})
-}
-changeUserType()
+
+  async function loginUser(){
+    // appending email and password to the APi call to authenticate user
+    var urllogin = new URLSearchParams()
+    urllogin.append("Email", document.querySelector('[email]').value)
+    urllogin.append("Password", document.querySelector('[password]').value)
+    try{
+    const res = await fetch("https://x8ki-letl-twmt.n7.xano.io/api:J2qjdVBc/auth/login",
+    {
+    method: 'POST',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        'Access-Control-Allow-Origin': window.location.href
+      },
+      body: urllogin
+    })
+    const data = await res.json()
+    // if anthentication is successful, create a cookie with the auth token with an expiration date of 1 month
+    if(res.status === 200){
+      const name = encodeURIComponent("lacToken");
+      const token = encodeURIComponent(data.authToken)
+      const currentDate = new Date();
+      currentDate.setMonth(currentDate.getMonth() + 1);
+      document.cookie = `${name}=${token}; expires=${currentDate}; path=/; domain=lake-atitlan-community.webflow.io`
+      location.href = "/tablero"
+      // if authentication is not successful, display error message  
+    } else document.querySelector('[invalidfields]').classList.add('show'); setTimeout(() => {document.querySelector('[invalidfields]').classList.remove('show')}, 3000)
+    } catch(error){
+    console.log('error:', error);
+    throw error;
+    }
+    }  
